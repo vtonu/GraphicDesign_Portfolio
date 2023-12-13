1aea879e1092139eb4d29480521edc2fb5cd3af5
vtonu <victor.tonu@yahoo.com>
2023-12-13 12:32:03 -0800

    Made it nicer
    Made it nicer by fixing margins and updating texts.

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,19 +8,45 @@ export default function IndexPage() {
   const { setColorScheme } = useMantineColorScheme();
   const computedColorScheme = useComputedColorScheme('light', { getInitialValueInEffect: true });
   return (
-    <Group mt={50} justify="center">
+    <Group mt={50} p="md" justify="center">
       <Grid>
-        <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>
+        <Grid.Col span={{ base: 10, md: 6, lg: 3 }}>
           <ActionIcon
             onClick={() => setColorScheme(computedColorScheme === 'light' ? 'dark' : 'light')}
             variant="default"
-            size="xl"
+            size="lg"
             aria-label="Toggle color scheme">
             <IconSun className={cx(classes.icon, classes.light)} stroke={1.5} />
             <IconMoon className={cx(classes.icon, classes.dark)} stroke={1.5} />
           </ActionIcon>
-          <Button variant="default">Cover Art</Button>
-          <Card shadow="sm" padding="lg" radius="md" withBorder>
+          <Group mb="xs" mt="xs">
+            <Button variant="default">Cover Art</Button>
+          </Group>
+          <Card shadow="sm" padding="md" radius="md" withBorder>
+            <Card.Section>
+              <Image src="https://i.postimg.cc/FsfGqJJQ/coverart.jpg" height={160} alt="Norway" />
+            </Card.Section>
+
+            <Group justify="space-between" mt="md" mb="xs">
+              <Text fw={500}>Norway Fjord Adventures</Text>
+              <Badge color="pink">Limited</Badge>
+            </Group>
+
+            <Text size="sm" c="dimmed">
+              With Fjord Tours you can explore more of the magical fjord landscapes with tours and
+              activities on and around the fjords of Norway
+            </Text>
+
+            <Button color="blue" fullWidth mt="md" radius="md">
+              Book classic tour now
+            </Button>
+          </Card>
+        </Grid.Col>
+        <Grid.Col span={{ base: 10, md: 6, lg: 3 }}>
+          <Group mb="xs" mt="xs">
+            <Button variant="default">Logos</Button>
+          </Group>
+          <Card shadow="sm" padding="md" radius="md" withBorder>
             <Card.Section>
               <Image
                 src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
@@ -44,9 +70,11 @@ export default function IndexPage() {
             </Button>
           </Card>
         </Grid.Col>
-        <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>
-          <Button variant="default">Logos</Button>
-          <Card shadow="sm" padding="lg" radius="md" withBorder>
+        <Grid.Col span={{ base: 10, md: 6, lg: 3 }}>
+          <Group mb="xs" mt="xs">
+            <Button variant="default">Hand Drawings</Button>
+          </Group>
+          <Card shadow="sm" padding="md" radius="md" withBorder>
             <Card.Section>
               <Image
                 src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
@@ -70,35 +98,11 @@ export default function IndexPage() {
             </Button>
           </Card>
         </Grid.Col>
-        <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>
-          <Button variant="default">Hand Drawings</Button>
-          <Card shadow="sm" padding="lg" radius="md" withBorder>
-            <Card.Section>
-              <Image
-                src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
-                height={160}
-                alt="Norway"
-              />
-            </Card.Section>
-
-            <Group justify="space-between" mt="md" mb="xs">
-              <Text fw={500}>Norway Fjord Adventures</Text>
-              <Badge color="pink">On Sale</Badge>
-            </Group>
-
-            <Text size="sm" c="dimmed">
-              With Fjord Tours you can explore more of the magical fjord landscapes with tours and
-              activities on and around the fjords of Norway
-            </Text>
-
-            <Button color="blue" fullWidth mt="md" radius="md">
-              Book classic tour now
-            </Button>
-          </Card>
-        </Grid.Col>
-        <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>
-          <Button variant="default">Illustrations</Button>
-          <Card shadow="sm" padding="lg" radius="md" withBorder>
+        <Grid.Col span={{ base: 10, md: 6, lg: 3 }}>
+          <Group mb="xs" mt="xs">
+            <Button variant="default">Illustrations</Button>
+          </Group>
+          <Card shadow="sm" padding="md" radius="md" withBorder>
             <Card.Section>
               <Image
                 src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
