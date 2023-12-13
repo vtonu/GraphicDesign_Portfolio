094a48d904a6e20a77b73c85378df60b389fd06b
vtonu <victor.tonu@yahoo.com>
2023-12-13 13:07:23 -0800

    Added favicon
    Added favicon & updated name of site.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,12 +7,12 @@ export default function App({ Component, pageProps }: any) {
   return (
     <MantineProvider theme={theme}>
       <Head>
-        <title>Mantine Template</title>
+        <title>Victor Tonu Portfolio</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
         />
-        <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="shortcut icon" href="./favicon.ico" />
       </Head>
       <Component {...pageProps} />
     </MantineProvider>
