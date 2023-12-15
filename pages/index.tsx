6b2b096cdbd12d42f1d96f36a7fa8d3cc12890cb
vtonu <victor.tonu@yahoo.com>
2023-12-15 13:06:09 -0800

    Removed all projects'
    Removed all projects' cards and grids except the main one and re-worked it so it's only showing my main react project with shadcn.

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,186 +10,52 @@ export default function IndexPage() {
   const computedColorScheme = useComputedColorScheme('light', { getInitialValueInEffect: true });
   return (
     <Group p="lg" justify="center">
-      <Grid justify="center" align="flex-start">
-        <Grid.Col span={{ base: 10, md: 6, lg: 3 }}>
-          <Card shadow="sm" padding="md" radius="md" withBorder>
-            <Group mb="xs" mt="xs">
-              <Button variant="default">Project 1</Button>
-              <ActionIcon
-                onClick={() => setColorScheme(computedColorScheme === 'light' ? 'dark' : 'light')}
-                variant="default"
-                size="lg"
-                aria-label="Toggle color scheme">
-                <IconSun className={cx(classes.icon, classes.light)} stroke={1.5} />
-                <IconMoon className={cx(classes.icon, classes.dark)} stroke={1.5} />
-              </ActionIcon>
-              <ActionIcon
-                variant="default"
-                size="lg"
-                aria-label="Toggle color scheme"
-                component="a"
-                href="https://github.com/vtonu"
-                target="_blank">
-                <IconBrandGithub></IconBrandGithub>
-              </ActionIcon>
-            </Group>
-            <Card.Section>
-              <Image src="https://i.postimg.cc/FsfGqJJQ/coverart.jpg" height={160} alt="Norway" />
-            </Card.Section>
-
-            <Group justify="space-between" mt="md" mb="xs">
-              <Text fw={500}>Graphic Design Portfolio</Text>
-              {/* <Badge color="green">NEW</Badge> */}
-            </Group>
-
-            <Text size="sm" c="dimmed">
-              Website made using Adobe Portfolio.
-            </Text>
-
-            <Button
+      <Grid justify="center" align="center">
+        <Card shadow="xs" padding="md" radius="md" withBorder>
+          <Group mb="xs" justify="center" align="center">
+            <ActionIcon
+              onClick={() => setColorScheme(computedColorScheme === 'light' ? 'dark' : 'light')}
+              variant="default"
+              size="lg"
+              aria-label="Toggle color scheme">
+              <IconSun className={cx(classes.icon, classes.light)} stroke={1.5} />
+              <IconMoon className={cx(classes.icon, classes.dark)} stroke={1.5} />
+            </ActionIcon>
+            <Button variant="default">React Project</Button>
+            <ActionIcon
+              variant="default"
+              size="lg"
+              aria-label="Toggle color scheme"
               component="a"
-              href="https://victortonu.myportfolio.com/"
-              target="_blank"
-              color="green"
-              fullWidth
-              mt="md"
-              radius="md">
-              See More
-            </Button>
-          </Card>
-        </Grid.Col>
-        <Grid.Col span={{ base: 10, md: 6, lg: 3 }}>
-          <Card shadow="sm" padding="md" radius="md" withBorder>
-            <Group mb="xs" mt="xs">
-              <Button variant="default">Project 2</Button>
-            </Group>
-            <Card.Section>
-              <Image
-                src="https://i.postimg.cc/xjHFT4P4/download.png"
-                height={160}
-                alt="3D Object"
-              />
-            </Card.Section>
+              href="https://github.com/vtonu/ShadCN_UI"
+              target="_blank">
+              <IconBrandGithub></IconBrandGithub>
+            </ActionIcon>
+          </Group>
+          <Card.Section>
+            <Image src="https://i.postimg.cc/SR84jrQh/download.png" height={300} alt="Project 1" />
+          </Card.Section>
 
-            <Group justify="space-between" mt="md" mb="xs">
-              <Text fw={500}>GAME 124 - Introduction to 3D with Maya</Text>
-              {/* <Badge color="pink">On Sale</Badge> */}
-            </Group>
+          <Group justify="space-between" mt="md" mb="xs">
+            <Text fw={500}>Graphic Design Portfolio</Text>
+            {/* <Badge color="green">NEW</Badge> */}
+          </Group>
 
-            <Text size="sm" c="dimmed">
-              This course is an introduction to the 3D content creation process as practiced in
-              Maya.
-            </Text>
+          <Text size="sm" c="dimmed">
+            A ShadCN Project using React, NextJS, Radix & TailwindCSS.
+          </Text>
 
-            <Button
-              component="a"
-              href="https://nuxt-ui-maya.vercel.app/"
-              target="_blank"
-              color="red"
-              fullWidth
-              mt="md"
-              radius="md">
-              See More
-            </Button>
-          </Card>
-        </Grid.Col>
-        <Grid.Col span={{ base: 10, md: 6, lg: 3 }}>
-          <Card shadow="sm" padding="md" radius="md" withBorder>
-            <Group mb="xs" mt="xs">
-              <Button variant="default">Project 3</Button>
-            </Group>
-            <Card.Section>
-              <Image
-                src="https://nuxt-ui-3-d.vercel.app/_nuxt/project10_10_10.0e7d5d83.jpg"
-                height={160}
-                alt="Norway"
-              />
-            </Card.Section>
-
-            <Group justify="space-between" mt="md" mb="xs">
-              <Text fw={500}>GAME 134 - 3D Materials & Textures</Text>
-              <Badge color="green">NEW</Badge>
-            </Group>
-
-            <Text size="sm" c="dimmed">
-              This course covers the elements of creating portfolio quality Material
-              Shaders/Textures applied to a 3D model.
-            </Text>
-
-            <Button
-              component="a"
-              href="https://nuxt-ui-3-d.vercel.app/"
-              target="_blank"
-              color="green"
-              fullWidth
-              mt="md"
-              radius="md">
-              See More
-            </Button>
-          </Card>
-        </Grid.Col>
-        <Grid.Col span={{ base: 10, md: 6, lg: 3 }}>
-          <Card shadow="sm" padding="md" radius="md" withBorder>
-            <Group mb="xs" mt="xs">
-              <Button variant="default">Project 4</Button>
-            </Group>
-            <Card.Section>
-              <Image
-                src="https://img.itch.zone/aW1nLzE0MjU0MTYwLmpwZw==/315x250%23c/stk3C%2B.jpg"
-                height={160}
-                alt="Norway"
-              />
-            </Card.Section>
-
-            <Group justify="space-between" mt="md" mb="xs">
-              <Text fw={500}>Itch.io Profile</Text>
-              {/* <Badge color="pink">On Sale</Badge> */}
-            </Group>
-
-            <Text size="sm" c="dimmed">
-              Itch.io page which contains all games from my college classes.
-            </Text>
-
-            <Button
-              component="a"
-              href="https://vtonu.itch.io/"
-              target="_blank"
-              color="green"
-              fullWidth
-              mt="md"
-              radius="md">
-              See More
-            </Button>
-          </Card>
-          <Card shadow="sm" padding="md" radius="md" withBorder mt="md">
-            <Group mb="xs" mt="xs">
-              <Button variant="default">Project 5</Button>
-            </Group>
-            <Card.Section>
-              <Image src="https://i.postimg.cc/xTzQXf6Q/download.png" height={160} alt="Norway" />
-            </Card.Section>
-
-            <Group justify="space-between" mt="md" mb="xs">
-              <Text fw={500}>Math Relationship</Text>
-              {/* <Badge color="pink">On Sale</Badge> */}
-            </Group>
-
-            <Text size="sm" c="dimmed">
-              A simple HTML, CSS, JS Project for my Math Class.
-            </Text>
-
-            <Button
-              component="a"
-              href="https://github.com/vtonu/MathRelationship_2"
-              target="_blank"
-              color="red"
-              fullWidth
-              mt="md"
-              radius="md">
-              See More
-            </Button>
-          </Card>
-        </Grid.Col>
+          <Button
+            component="a"
+            href="https://shad-cn-ui.vercel.app/"
+            target="_blank"
+            variant="default"
+            fullWidth
+            mt="md"
+            radius="md">
+            See More
+          </Button>
+        </Card>
       </Grid>
     </Group>
   );
